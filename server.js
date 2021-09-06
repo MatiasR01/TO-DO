5b2f0a01199ace658db8f21fa3c7d79655ff0d32
MatiasR01 <romeromat2001@gmail.com>
2021-09-06 20:45:50 -0300

    herokuv4

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/trabajonro2'));
+app.use(express.static('./dist/TO-DO'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/trabajonro2/'}),
+    res.sendFile('index.html', {root: 'dist/TO-DO/'}),
 );
 
 // Start the app by listening on the default Heroku port
