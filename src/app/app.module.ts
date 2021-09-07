5356a89b624320172e8bc2d6fa5e66079762cc09
MatiasR01 <romeromat2001@gmail.com>
2021-09-07 12:55:09 -0300

    codigov3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppTODOComponent } from './componentes/app-to-do/app-to-do.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppTODOComponent
   ],
   imports: [
     BrowserModule,
